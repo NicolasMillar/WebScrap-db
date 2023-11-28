668e38469671f68972d375001defcf6759d1e747
Nicolas <nicolas.millar.millar@gmail.com>
2023-11-28 19:55:09 -0300

    obtiene el html de la pagina

--- webscraper.py
+++ webscraper.py
@@ -1,2 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
+
+website = 'https://www.magicsur.cl/66-digimon-card-game-chile'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+result = requests.get(website, headers=headers)
+content = result.text
+
+soup = BeautifulSoup(content, 'lxml')
+print(soup.prettify())
