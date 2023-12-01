d0026946810387f1d38c8d724c687bf69fc67684
Nicolas <nicolas.millar.millar@gmail.com>
2023-12-01 19:56:07 -0300

    agregando coneccion a la bd

--- populateDB.py
+++ populateDB.py
@@ -1,7 +1,32 @@
+import psycopg2
 from webscraper import scrap_magic, scrap_ThirdImpact, scrap_magicChile
 
-scraped_productsMagicSur = scrap_magic()
-scraped_productsThirdImpact = scrap_ThirdImpact()
-scraped_productsmagicChile = scrap_magicChile()
+dbname = 'wgezdikj'
+user = 'wgezdikj'
+password = 'Pp96GdIcpkrJNk2Dxp7B5eQmhrNkgTCH'
+host = 'bubble.db.elephantsql.com'
+port = '5432'
 
-print(scraped_productsmagicChile)
+try:
+    connection = psycopg2.connect(
+        dbname=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port
+    )
+
+    print("Conexión exitosa!")
+
+
+except Exception as e:
+    print(f"Error de conexión: {e}")
+
+finally:
+    if connection:
+        connection.close()
+        print("Conexión cerrada.")
+
+#scraped_productsMagicSur = scrap_magic()
+#scraped_productsThirdImpact = scrap_ThirdImpact()
+#scraped_productsmagicChile = scrap_magicChile()
